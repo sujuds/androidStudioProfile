1b93b9ee26d8f70a3e5799c985046fd02a67fb58
sujuds <satwikayana@gmail.com>
2019-12-24 13:54:45 +0800

    Localization

--- app/src/main/java/com/example/sujudprofile/Profile.java
+++ app/src/main/java/com/example/sujudprofile/Profile.java
@@ -1,28 +1,50 @@
 package com.example.sujudprofile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Profile extends AppCompatActivity implements View.OnClickListener {
+public class Profile extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        ImageButton btnBack = findViewById(R.id.btn_back);
-        btnBack.setOnClickListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationIcon(R.drawable.ic_chevron_left_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_back){
-            Intent intent = new Intent(this, MainActivity.class);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.setting){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
             startActivity(intent);
         }
+        return super.onOptionsItemSelected(item);
     }
 }
